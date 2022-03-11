df3f12514ce3fe256ba04e01e52eec0a3184d938
rkajitani <rei.kajitani@gmail.com>
2022-03-11 10:47:14 +0900

    fix FALCON-Unzip test script.

--- test_data/FALCON-Unzip_test.sh
+++ test_data/FALCON-Unzip_test.sh
@@ -1,6 +1,6 @@
 greenhill \
 	-cph FALCON-Unzip_result/cns_p_ctg.fa FALCON-Unzip_result/cns_h_ctg.fa \
 	-p reads/longread.fq,gz \
-	-HIC reads/HIC_1.fq.gz reads/HIC_2.fq.gz \ 
+	-HIC reads/HIC_1.fq.gz reads/HIC_2.fq.gz \
 	-o FALCON-Unzip \
 	>FALCON-Unzip.log 2>&1
